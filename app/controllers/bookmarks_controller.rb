094f5c679fc85be142f6ffbc3cb250dfb7e7d616
Diane Atticus <diane.atticus@laposte.net>
2024-11-14 17:04:56 +0100

    bookmark ajoutés

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+
+  def new
+    # récupère la list qui est liée
+    @list = List.find(params[:list_id])
+    # coquille vide d'un bookmark
+    @bookmark = Bookmark.new
+    # récupération de toutes les movies
+    # choix du user sur la movie dans la view et récupérationd e l'id
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    # la list liée au bookmark créée sera celui identifié avant
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
