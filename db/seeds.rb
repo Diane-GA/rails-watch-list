a8eb65c185ffac71d082d1b02bcdd5fef8e6844b
Diane Atticus <diane.atticus@laposte.net>
2024-11-14 13:01:23 +0100

    routes faites

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+# Seed de 10 movies
+# 10.times do
+#     rate = rand(0..10)
+#     poster = ["../app/assets/images/aditya-saxena-tj24rcDcmkY-unsplash.jpg","../app/assets/images/austin-chan-ukzHlkoz1IE-unsplash.jpg", "../app/assets/images/casey-horner-0D8_tYHIy0o-unsplash.jpg", "../app/assets/images/freestocks-Qx5QNarbrUM-unsplash.jpg", "../app/assets/images/gaspar-zaldo-Z9NAI0mf1KA-unsplash.jpg", "../app/assets/images/gio-bartlett-2-C_Sa5wZZA-unsplash.jpg", "../app/assets/images/hal-gatewood-OgvqXGL7XO4-unsplash.jpg", "../app/assets/images/hans-eiskonen--RQ8tGcn2EY-unsplash.jpg", "../app/assets/images/history-in-hd-e5eDHbmHprg-unsplash.jpg", "../app/assets/images/jared-rice-NTyBbu66_SI-unsplash.jpg", "../app/assets/images/jr-korpa-JKRL7ofWWAA-unsplash.jpg", "../app/assets/images/loren-cutler-DLEvzWzKtoM-unsplash.jpg", "../app/assets/images/nathan-wright-igpwuxZofgo-unsplash.jpg", "../app/assets/images/peter-larsen-vGzmLshPjbc-unsplash.jpg", "../app/assets/images/stefano-pollio-ZC0EbdLC8G0-unsplash.jpg", "../app/assets/images/steven-beyer-xsZUrBBcUk0-unsplash.jpg", "../app/assets/images/vitaliy-shevchenko-vVcxBZTx37g-unsplash.jpg"].sample
+#     Movie.create!(title: Faker::Movie.title, overview: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, poster_url: poster, rating: rate)
 #   end
+
+# Seed de 7 catégories
+# List.create(name: "Romance")
+# List.create(name: "Horror")
+# List.create(name: "Action")
+# List.create(name: "Science-fiction")
+# List.create(name: "Fantasy")
+# List.create(name: "Thriller")
+# List.create(name: "Spy")
+# List.create(name: "Documentary")
