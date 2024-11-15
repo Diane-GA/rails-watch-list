9f329af91c49770f5ad953011e626412fcf5ebaf
Diane Atticus <diane.atticus@laposte.net>
2024-11-15 15:46:17 +0100

    truc

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,30 @@
+
 # Seed de 10 movies
-# 10.times do
-#     rate = rand(0..10)
-#     poster = ["../app/assets/images/aditya-saxena-tj24rcDcmkY-unsplash.jpg","../app/assets/images/austin-chan-ukzHlkoz1IE-unsplash.jpg", "../app/assets/images/casey-horner-0D8_tYHIy0o-unsplash.jpg", "../app/assets/images/freestocks-Qx5QNarbrUM-unsplash.jpg", "../app/assets/images/gaspar-zaldo-Z9NAI0mf1KA-unsplash.jpg", "../app/assets/images/gio-bartlett-2-C_Sa5wZZA-unsplash.jpg", "../app/assets/images/hal-gatewood-OgvqXGL7XO4-unsplash.jpg", "../app/assets/images/hans-eiskonen--RQ8tGcn2EY-unsplash.jpg", "../app/assets/images/history-in-hd-e5eDHbmHprg-unsplash.jpg", "../app/assets/images/jared-rice-NTyBbu66_SI-unsplash.jpg", "../app/assets/images/jr-korpa-JKRL7ofWWAA-unsplash.jpg", "../app/assets/images/loren-cutler-DLEvzWzKtoM-unsplash.jpg", "../app/assets/images/nathan-wright-igpwuxZofgo-unsplash.jpg", "../app/assets/images/peter-larsen-vGzmLshPjbc-unsplash.jpg", "../app/assets/images/stefano-pollio-ZC0EbdLC8G0-unsplash.jpg", "../app/assets/images/steven-beyer-xsZUrBBcUk0-unsplash.jpg", "../app/assets/images/vitaliy-shevchenko-vVcxBZTx37g-unsplash.jpg"].sample
-#     Movie.create!(title: Faker::Movie.title, overview: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, poster_url: poster, rating: rate)
-#   end
+# on clean la DB de movies et bookmarks (car dependent destroy)
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+
+# on créé 10 movies
+puts "Creating movies..."
+10.times do
+    rate = rand(0..10)
+    poster = ["aditya-saxena-tj24rcDcmkY-unsplash.jpg","austin-chan-ukzHlkoz1IE-unsplash.jpg", "casey-horner-0D8_tYHIy0o-unsplash.jpg", "freestocks-Qx5QNarbrUM-unsplash.jpg", "gaspar-zaldo-Z9NAI0mf1KA-unsplash.jpg", "gio-bartlett-2-C_Sa5wZZA-unsplash.jpg", "hal-gatewood-OgvqXGL7XO4-unsplash.jpg", "hans-eiskonen--RQ8tGcn2EY-unsplash.jpg", "history-in-hd-e5eDHbmHprg-unsplash.jpg", "jared-rice-NTyBbu66_SI-unsplash.jpg", "jr-korpa-JKRL7ofWWAA-unsplash.jpg", "loren-cutler-DLEvzWzKtoM-unsplash.jpg", "nathan-wright-igpwuxZofgo-unsplash.jpg", "peter-larsen-vGzmLshPjbc-unsplash.jpg", "stefano-pollio-ZC0EbdLC8G0-unsplash.jpg", "steven-beyer-xsZUrBBcUk0-unsplash.jpg", "vitaliy-shevchenko-vVcxBZTx37g-unsplash.jpg"].sample
+    Movie.create!(title: Faker::Movie.title, overview: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, poster_url: poster, rating: rate)
+  end
 
 # Seed de 7 catégories
-# List.create(name: "Romance")
-# List.create(name: "Horror")
-# List.create(name: "Action")
-# List.create(name: "Science-fiction")
-# List.create(name: "Fantasy")
-# List.create(name: "Thriller")
-# List.create(name: "Spy")
-# List.create(name: "Documentary")
+# on clean la DB
+puts "Cleaning database..."
+List.destroy_all
+
+# on créé 8 lists
+puts "Creating lists..."
+List.create(name: "Romance")
+List.create(name: "Horror")
+List.create(name: "Action")
+List.create(name: "Science-fiction")
+List.create(name: "Fantasy")
+List.create(name: "Thriller")
+List.create(name: "Spy")
+List.create(name: "Documentary")
